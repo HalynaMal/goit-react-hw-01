0e96e0247ad6ee730b0643214aa22df29ad9168f
HalynaMal <dguoit@ukr.net>
2024-03-06 22:53:14 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import './App.css';
 import Profile from './components/Profile/Profile';
+import userData from './components/Profile/userData.json';
 
-function App() {
-  return (
-    <Profile />
-  );
-}
+const App = () => {
+ return (
+ <>
+ <Profile
+  name={userData.username}
+  tag={userData.tag}
+  location={userData.location}
+  image={userData.avatar}
+  stats={userData.stats}
+ />
+ </>
+ );
+};
 
 export default App;
